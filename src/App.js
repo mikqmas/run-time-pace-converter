18009bd494115d36b48289dd01f8dfb8898b5934
Sam Kim <samkim@Sams-MBP.attlocal.net>
2020-06-27 19:01:37 -0700

    mend

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {convertSecToTime, convertTimeToSec, displayTime, fixInputTime} from './utils/calculator.js';
 
